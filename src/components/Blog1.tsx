ee94f9be9675f8cef39245a8ec33569cb33c147c
freelancing with programming <iosattbsoft2021@gmail.com>
2024-11-26 17:47:33 +0500

    cccc

--- src/components/Blog1.tsx
+++ src/components/Blog1.tsx
@@ -1,5 +1,4 @@
 import Link from 'next/link';
-import data from '../public/blog.json';
 
 const Blog = async () => {
   // Simulate fetching the JSON file
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,7 +6,7 @@ export default function Home(){
                 <Image src={Logo} width={200} height={200} alt="c"/>    
             </div>
             <div className="text-white">
-            <h1 className="text-bold text-6xl">Sidney Liebrand's blog</h1>
+            <h1 className="text-bold text-6xl">Sidney Liebrands blog</h1>
             <h4 className="text-medium text-xl">My name is Sidney Liebrand. I am a Software developer specialized in front-end development with experience in JS, React, Ruby, Rails, Lua, PHP, and more. This is the place where I publish new blog posts and perhaps a tool or two in the future.
             </h4>
             </div>
